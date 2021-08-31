4125718789fde3837f97e9b766293da8ec4faf94
RENZOZ78 <didierbap@gmail.com>
2021-08-31 17:22:51 +0200

    SECONDE BANNIERE & TEXTE

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-$document.on    
+
+// image fermante elkko lightbox
+$(document).on('click', '[data-toggle="lightbox"]', function(event){
+    event.preventDefault();
+    $(this).ekkoLightbox();
+}) 
+
+
+// carousel de texte = enlever la pose sur le texte pointé
+$('.carousel').carousel({
+    interval: 2500,
+    pause: 'null'
+})
+
